652c87d494c9b957bb0bf27cc0aa632be8ca6bc6
Eduardo Milpas <eduardomilpas@gmail.com>
2021-07-21 17:42:01 -0400

    Update Sum.java

--- src/main/java/com/Sum.java
+++ src/main/java/com/Sum.java
@@ -1,6 +1,7 @@
 package com;
 
 import java.util.Arrays;
+import utils.Zero;
 
 /*
 Calculate the sum of the elements in an array, keeping in mind that some of those integers may be quite large.
@@ -11,96 +12,135 @@ Output: String with the sum of the elements in the array.
 */
 public class Sum {
 
-	public String aVeryBigSum(int[] numbers) {
-		int[] total = new int[getMaxLength(numbers) + 1];
+    private Zero zero;
 
-		for (int number : numbers) {
-			int[] ns = setupArraySize(splitIntoIndividualNumbers(number), total.length);
+    public Sum() {
+        this.zero = new Zero();
+    }
 
-			for (int i = total.length - 1; i > 0; i--) {
-				sum(total, ns[i], i);
-			}
-		}
-		return parseToString(total);
-	}
+    public String summarize(String value1, String value2) {
+        if (value1.length() > value2.length()) {
+            value2 = zero.getZeroes(value1.length() - value2.length()) + value2;
+        } else {
+            value1 = zero.getZeroes(value2.length() - value1.length()) + value1;
+        }
 
-	private void sum(int[] total, int n, int i) {
-		int sumTotalPlusN = total[i] + n;
-		if (sumTotalPlusN < 10 || i == 0) {
-			total[i] = sumTotalPlusN;
-		} else {
-			int[] tPlusN = splitIntoIndividualNumbers(sumTotalPlusN);
-			total[i] = tPlusN[1];
-			sum(total, tPlusN[0], i - 1);
-		}
+        char[] val1 = value1.toCharArray();
+        char[] val2 = value2.toCharArray();
+        int extra = 0, v1, v2 = 0;
+        String total = "", result = "";
 
-	}
+        for (int i = val1.length - 1; i >= 0; i--) {
+            v1 = Character.getNumericValue(val1[i]);
+            v2 = Character.getNumericValue(val2[i]);
+            result = String.valueOf(v1 + v2 + extra);
 
-	private String parseToString(int[] array) {
-		array = deleteZeroesOnLeft(array);
-		StringBuilder sb = new StringBuilder();
+            if (result.length() == 1) {
+                total = result + total;
+                extra = 0;
+            } else {
+                total = result.toCharArray()[1] + total;
+                extra = Character.getNumericValue(result.toCharArray()[0]);
+            }
+        }
 
-		for (int i : array) {
-			sb.append(i);
-		}
+        total = extra + total;
+        total = zero.removeZeroesToTheLeft(total);
 
-		return sb.toString();
-	}
+        return total;
+    }
 
-	private int[] deleteZeroesOnLeft(int[] array) {
-		int lastZeroIndex = -1;
-		
-		for (int i = 0; i < array.length; i++) {
-			if (array[i] == 0) {
-				lastZeroIndex = i;
-			} else {
-				break;
-			}
-		}
 
-		lastZeroIndex = lastZeroIndex > -1 ? lastZeroIndex + 1 : 0;
 
-		return Arrays.copyOfRange(array, lastZeroIndex, array.length);
-	}
+    public String aVeryBigSum(int[] numbers) {
+        int[] total = new int[getMaxLength(numbers) + 1];
 
-	private int[] setupArraySize(int[] array, int n) {
-		int[] newArray = new int[array.length > n ? array.length : n];
+        for (int number : numbers) {
+            int[] ns = setupArraySize(splitIntoIndividualNumbers(number), total.length);
 
-		for (int i = 0; i < array.length; i++) {
-			newArray[newArray.length - (i + 1)] = array[array.length - (i + 1)];
-		}
+            for (int i = total.length - 1; i > 0; i--) {
+                sum(total, ns[i], i);
+            }
+        }
+        return parseToString(total);
+    }
 
-		return newArray;
-	}
+    private void sum(int[] total, int n, int i) {
+        int sumTotalPlusN = total[i] + n;
+        if (sumTotalPlusN < 10 || i == 0) {
+            total[i] = sumTotalPlusN;
+        } else {
+            int[] tPlusN = splitIntoIndividualNumbers(sumTotalPlusN);
+            total[i] = tPlusN[1];
+            sum(total, tPlusN[0], i - 1);
+        }
+    }
 
-	private int[] splitIntoIndividualNumbers(int number) {
-		int length = getLength(number);
-		int[] splitNumber = new int[length];
-		int index = length - 1;
+    private String parseToString(int[] array) {
+        array = deleteZeroesOnLeft(array);
+        StringBuilder sb = new StringBuilder();
 
-		while (number / 10 > 0) {
-			splitNumber[index--] = number % 10;
-			number /= 10;
-		}
-		splitNumber[index] = number;
+        for (int i : array) {
+            sb.append(i);
+        }
 
-		return splitNumber;
-	}
+        return sb.toString();
+    }
 
-	private int getMaxLength(int[] numbers) {
-		int maxLength = 0;
-		int numberLength = 0;
+    private int[] deleteZeroesOnLeft(int[] array) {
+        int lastZeroIndex = -1;
 
-		for (int number : numbers) {
-			numberLength = getLength(number);
-			maxLength = numberLength > maxLength ? numberLength : maxLength;
-		}
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 0) {
+                lastZeroIndex = i;
+            } else {
+                break;
+            }
+        }
 
-		return maxLength;
-	}
+        lastZeroIndex = lastZeroIndex > -1 ? lastZeroIndex + 1 : 0;
 
-	private int getLength(int number) {
-		return number == 0 ? 1 : (int) (Math.log10(number) + 1);
-	}
+        return Arrays.copyOfRange(array, lastZeroIndex, array.length);
+    }
+
+    private int[] setupArraySize(int[] array, int n) {
+        int[] newArray = new int[array.length > n ? array.length : n];
+
+        for (int i = 0; i < array.length; i++) {
+            newArray[newArray.length - (i + 1)] = array[array.length - (i + 1)];
+        }
+
+        return newArray;
+    }
+
+    private int[] splitIntoIndividualNumbers(int number) {
+        int length = getLength(number);
+        int[] splitNumber = new int[length];
+        int index = length - 1;
+
+        while (number / 10 > 0) {
+            splitNumber[index--] = number % 10;
+            number /= 10;
+        }
+        splitNumber[index] = number;
+
+        return splitNumber;
+    }
+
+    private int getMaxLength(int[] numbers) {
+        int maxLength = 0;
+        int numberLength = 0;
+
+        for (int number : numbers) {
+            numberLength = getLength(number);
+            maxLength = numberLength > maxLength ? numberLength : maxLength;
+        }
+
+        return maxLength;
+    }
+
+    private int getLength(int number) {
+        return number == 0 ? 1 : (int) (Math.log10(number) + 1);
+    }
 
 }
