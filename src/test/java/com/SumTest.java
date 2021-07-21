4e6c393f907c4813226cab2795c2959129324a91
Eduardo Milpas <eduardomilpas@gmail.com>
2021-07-21 17:43:28 -0400

    Update SumTest.java

--- src/test/java/com/SumTest.java
+++ src/test/java/com/SumTest.java
@@ -1,11 +1,12 @@
 package com;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
-
-import java.util.*;
-
-import static org.junit.Assert.assertEquals;
 
 /*
 Calculate the sum of the elements in an array, keeping in mind that some of those integers may be quite large.
@@ -29,8 +30,18 @@ public class SumTest {
     @Test
     public void sum_whenInvoked_returnsSumOfElements() {
         values.forEach(element -> {
-            assertEquals("Error testing:" + Arrays.toString(element.getNumbers()), element.getTotal(), subject.aVeryBigSum(element.getNumbers()));
+            assertEquals("Error testing:" + Arrays.toString(element.getNumbers()), element.getTotal(),
+                    subject.aVeryBigSum(element.getNumbers()));
         });
+    }
+
+    @Test
+    public void summarize() {
+        assertEquals("370", subject.summarize("123", "247"));
+        assertEquals("1111111110", subject.summarize("123456789", "987654321"));
+        assertEquals("1328819997", subject.summarize("1258965874", "69854123"));
+        assertEquals("1000066666665", subject.summarize("999999999999", "66666666"));
+        assertEquals("3", subject.summarize("01", "00000000000002"));
     }
 
     private void initValues() {
@@ -43,6 +54,7 @@ public class SumTest {
     }
 
     private class SumElement {
+
         private String total;
         private int[] numbers;
 
